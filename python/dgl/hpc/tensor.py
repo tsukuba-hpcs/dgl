f76517d0d2e9c1bcc1ed783dfd7c14ee8af0df03
KoyamaSohei <koyamaso0309@gmail.com>
2021-09-24 01:45:08 +0900

    rm init_func

--- python/dgl/hpc/tensor.py
+++ python/dgl/hpc/tensor.py
@@ -5,9 +5,14 @@ from .context import ManagerContext
 from .partition import PartitionPolicy
 from typing import Tuple, Callable, Type
 
-__all__ = ['serveTensor']
+__all__ = ['serveTensor', 'TensorShard']
+
+class TensorShard:
+  def __init__(self):
+    pass
 
 def serveTensor(mcontext: ManagerContext, name: str, shape: Tuple[int, ...], dtype: Type[F.dtype],
-  policy: Type[PartitionPolicy], init_func: Callable[[Tuple[int, ...], Type[F.dtype]], None]):
+  policy: Type[PartitionPolicy]) -> TensorShard:
   print('serveTensor called')
   print('shape', shape)
+  return TensorShard()
--- tests/hpc/test_manager.py
+++ tests/hpc/test_manager.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
   mcontext = dgl.hpc.ManagerContext()
   print('mcontext.rank', mcontext.rank)
   print('mcontext.size', mcontext.size)
-  init_func = lambda shape, dtype: th.zeros(shape, dtype)
-  dgl.hpc.serveTensor(mcontext, "foo", (100,100), th.float64, dgl.hpc.ModuloPolicy, init_func)
+  fooshard = dgl.hpc.serveTensor(mcontext, "foo", (100,100), th.float64, dgl.hpc.ModuloPolicy)
   mcontext.launchWorker(1, "python", "tests/hpc/test_worker.py")
   mcontext.serve()
