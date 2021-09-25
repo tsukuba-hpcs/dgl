ad0155273a199c81fb6f1bad9a0bf828d1a8e307
KoyamaSohei <koyamaso0309@gmail.com>
2021-09-25 09:23:55 +0900

    use ShardPolicy

--- python/dgl/hpc/shard.py
+++ python/dgl/hpc/shard.py
@@ -20,7 +20,11 @@ class ShardPolicy(ABC):
     self.manager_size = manager_size
 
   @abstractmethod
-  def __getitem__(self, index: int) -> int:
+  def __getitem__(self, index: int) -> Tuple[int, int]:
+    pass
+
+  @abstractmethod
+  def get_local_row_size(self, rank: int) -> int:
     pass
 
 class ModuloPolicy(ShardPolicy):
@@ -28,34 +32,48 @@ class ModuloPolicy(ShardPolicy):
   def __init__(self, row_size: int, manager_size: int):
     super().__init__(row_size, manager_size)
 
-  def __getitem__(self, index: int) -> int:
+  def __getitem__(self, index: int) -> Tuple[int, int]:
     assert index < self.row_size, "index is out of range"
     assert 0 <= index, "index is out of range"
-    return index % self.manager_size
+    return (index % self.manager_size, index // self.manager_size)
+
+  def get_local_row_size(self, rank: int) -> int:
+    a = self.row_size // self.manager_size
+    b = 1 if rank < self.row_size % self.manager_size else 0
+    return a + b
 
 @register_object('hpc.Shard')
 class Shard(ObjectBase):
-
-  def __init__(self):
+  rank: int
+  size: int
+  def __init__(self, rank: int, size: int):
     self.__init_handle_by_constructor__(
       _CAPI_HPCCreateShard
     )
+    self.rank = rank
+    self.size = size
 
 class TensorShard:
   id: int
   name: str
   shape: Tuple[int, ...]
   dtype: Type[F.dtype]
-  def __init__(self, id: int, name: str, shape: Tuple[int, ...], dtype: Type[F.dtype]):
+  policy: ShardPolicy
+  def __init__(self, id: int, name: str, shape: Tuple[int, ...], dtype: Type[F.dtype],
+    policy: ShardPolicy):
     self.id = id
     self.name = name
     self.shape = shape
     self.dtype = dtype
+    self.policy = policy
 
 def createTensor(shard: Shard, name: str, shape: Tuple[int, ...], dtype: Type[F.dtype],
   policy: Type[ShardPolicy]) -> TensorShard:
-  tensor = empty(shape, F.reverse_data_type_dict[dtype])
+  (row_size, *col_sizes) = shape
+  sp: ShardPolicy = policy(row_size, shard.size)
+  local_shape = (sp.get_local_row_size(shard.rank), *col_sizes)
+  tensor = empty(local_shape, F.reverse_data_type_dict[dtype])
   id = _CAPI_HPCRegisterTensor(shard, name, tensor)
-  return TensorShard(id, name, shape, dtype)
+  return TensorShard(id, name, shape, dtype, sp)
 
 _init_api("dgl.hpc.shard")
--- tests/hpc/test_manager.py
+++ tests/hpc/test_manager.py
@@ -3,12 +3,10 @@ import dgl
 
 if __name__ == "__main__":
   mcontext = dgl.hpc.ManagerContext()
-  print('mcontext.rank', mcontext.rank)
-  print('mcontext.size', mcontext.size)
-  shard = dgl.hpc.Shard()
-  fooshard = dgl.hpc.createTensor(shard, "foo", (100,100), th.float64, dgl.hpc.ModuloPolicy)
+  shard = dgl.hpc.Shard(mcontext.rank, mcontext.size)
+  fooshard = dgl.hpc.createTensor(shard, "foo", (101,100), th.float64, dgl.hpc.ModuloPolicy)
   assert fooshard.id == 0
-  barshard = dgl.hpc.createTensor(shard, "bar", (100,100), th.float64, dgl.hpc.ModuloPolicy)
+  barshard = dgl.hpc.createTensor(shard, "bar", (102,100), th.float64, dgl.hpc.ModuloPolicy)
   assert barshard.id == 1
   mcontext.launchWorker(1, "python", "tests/hpc/test_worker.py")
   mcontext.serve()
