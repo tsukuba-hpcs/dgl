be59105c3f2f8cfef87d887eae4bd85bcfe202a1
KoyamaSohei <koyamaso0309@gmail.com>
2021-11-12 15:50:10 +0900

    add __gather_workeraddr

--- python/dgl/distributedv2/context.py
+++ python/dgl/distributedv2/context.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
-from ctypes import *
+from ctypes import c_ubyte, c_void_p, POINTER, cast
 from .._ffi.object import register_object, ObjectBase
 from .._ffi.function import _init_api
 
@@ -16,10 +16,24 @@ class Context(ObjectBase):
             rank,
             size
         )
-        addr = _CAPI_UCXGetWorkerAddr(self)
-        print('addr=', addr)
-        len = _CAPI_UCXGetWorkerAddrlen(self)
-        print('addrlen=', len)
+        addrs = self.__gather_workeraddr(size)
+        print(addrs)
+
+    def __gather_workeraddr(self, size):
+        comm = MPI.COMM_WORLD
+
+        # exchange worker address
+        addr= _CAPI_UCXGetWorkerAddr(self)
+        addrlen = _CAPI_UCXGetWorkerAddrlen(self)
+        UByteArr = c_ubyte * addrlen
+        UByteArrPtr = POINTER(UByteArr)
+        addr = cast(addr, UByteArrPtr)
+        addr = bytearray(addr.contents)
+        s_msg = [addr, addrlen, MPI.BYTE]
+        addrs = bytearray(addrlen * size)
+        r_msg = [addrs, addrlen, MPI.BYTE]
+        comm.Allgather(s_msg, r_msg)
+        return addrs
 
     def __del__(self):
         _CAPI_UCXFinalizeContext(self)
