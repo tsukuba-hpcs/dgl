8fdabc2c6338b226aadb53af4374a5d9a446a387
KoyamaSohei <koyamaso0309@gmail.com>
2021-11-12 16:01:19 +0900

    fix api

--- python/dgl/distributedv2/context.py
+++ python/dgl/distributedv2/context.py
@@ -10,18 +10,18 @@ __all__ = [
 
 @register_object('ucx.Context')
 class Context(ObjectBase):
-    def __init__(self, rank: int, size: int):
+    def __init__(self, comm = MPI.COMM_WORLD):
+        rank = comm.Get_rank()
+        size = comm.Get_size()
         self.__init_handle_by_constructor__(
             _CAPI_UCXCreateContext,
             rank,
             size
         )
-        addrs = self.__gather_workeraddr(size)
+        addrs = self.__gather_workeraddr(comm)
         print(addrs)
 
-    def __gather_workeraddr(self, size):
-        comm = MPI.COMM_WORLD
-
+    def __gather_workeraddr(self, comm):
         # exchange worker address
         addr= _CAPI_UCXGetWorkerAddr(self)
         addrlen = _CAPI_UCXGetWorkerAddrlen(self)
@@ -30,7 +30,7 @@ class Context(ObjectBase):
         addr = cast(addr, UByteArrPtr)
         addr = bytearray(addr.contents)
         s_msg = [addr, addrlen, MPI.BYTE]
-        addrs = bytearray(addrlen * size)
+        addrs = bytearray(addrlen * comm.Get_size())
         r_msg = [addrs, addrlen, MPI.BYTE]
         comm.Allgather(s_msg, r_msg)
         return addrs
