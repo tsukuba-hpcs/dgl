92987e45df54f2218ab8b603a54613aa32aa3a1c
KoyamaSohei <koyamaso0309@gmail.com>
2021-12-31 03:35:31 +0900

    avoid to use mpi4py in global

--- python/dgl/distributedv2/comm.py
+++ python/dgl/distributedv2/comm.py
@@ -1,39 +1,38 @@
-from mpi4py import MPI
 from ctypes import c_ubyte, c_void_p, POINTER, cast
 from .._ffi.object import register_object, ObjectBase
 from .._ffi.function import _init_api
 
 __all__ = [
     'Communicator',
+    'allgather',
 ]
 
+def allgather(comm, addr, addrlen):
+    from mpi4py import MPI
+    UByteArr = c_ubyte * addrlen
+    UByteArrPtr = POINTER(UByteArr)
+    addr = cast(addr, UByteArrPtr)
+    addr = bytearray(addr.contents)
+    s_msg = [addr, addrlen, MPI.BYTE]
+    addrs = bytearray(addrlen * comm.Get_size())
+    r_msg = [addrs, addrlen, MPI.BYTE]
+    comm.Allgather(s_msg, r_msg)
+    return addrs
 
 @register_object('distributedv2.Communicator')
 class Communicator(ObjectBase):
-    def __init__(self, comm = MPI.COMM_WORLD):
+    def __init__(self, comm):
         self.rank = comm.Get_rank()
         self.size = comm.Get_size()
-        self.mpi = comm
         self.__init_handle_by_constructor__(
             _CAPI_DistV2CreateCommunicator,
             self.rank,
             self.size
         )
 
-    def allgather(self, addr, addrlen):
-        UByteArr = c_ubyte * addrlen
-        UByteArrPtr = POINTER(UByteArr)
-        addr = cast(addr, UByteArrPtr)
-        addr = bytearray(addr.contents)
-        s_msg = [addr, addrlen, MPI.BYTE]
-        addrs = bytearray(addrlen * self.size)
-        r_msg = [addrs, addrlen, MPI.BYTE]
-        self.mpi.Allgather(s_msg, r_msg)
-        return addrs
-
-    def create_endpoints(self):
+    def create_endpoints(self, comm):
         addr, addrlen = _CAPI_DistV2CreateWorker(self)
-        worker_addrs = self.allgather(addr, addrlen)
+        worker_addrs = allgather(comm, addr, addrlen)
         _CAPI_DistV2CreateEndpoints(self, worker_addrs)
 
 _init_api("dgl.distributedv2", __name__)
--- python/dgl/distributedv2/dataloader.py
+++ python/dgl/distributedv2/dataloader.py
@@ -1,9 +1,8 @@
 import numpy as np
 import dgl
 from array import array
-from mpi4py import MPI
 from ..heterograph import DGLBlock
-from .comm import Communicator
+from .comm import Communicator, allgather
 from .._ffi.object import register_object, ObjectBase
 from .._ffi.function import _init_api
 from ctypes import c_ubyte, c_void_p, POINTER, cast, sizeof
@@ -39,7 +38,8 @@ class NodeDataLoader(ObjectBase):
     2. each process calculate where to shard temporary edges.
     3. exchange temporary edges by Alltoallv()
     """
-    def __create_edgeshard(self, edges):
+    def __create_edgeshard(self, comm, edges):
+        from mpi4py import MPI
         temp_edges = self.__load_temp_edge(edges)
         vals, cnts = self.__count_dest_rank(temp_edges)
         s_counts = array('Q', [0] * self.comm.size)
@@ -49,7 +49,7 @@ class NodeDataLoader(ObjectBase):
         r_counts = array('Q', [0] * self.comm.size)
         s_msg = [s_counts, 8, MPI.BYTE]
         r_msg = [r_counts, 8, MPI.BYTE]
-        self.comm.mpi.Alltoall(s_msg, r_msg)
+        comm.Alltoall(s_msg, r_msg)
 
         # reuse s_counts as byte counts
         s_counts = [c * (edges.itemsize * 2) for c in s_counts]
@@ -75,7 +75,7 @@ class NodeDataLoader(ObjectBase):
 
         s_msg = [temp_edges, (s_counts, s_displs), MPI.BYTE]
         r_msg = [recv_edges, (r_counts, r_displs), MPI.BYTE]
-        self.comm.mpi.Alltoallv(s_msg, r_msg)
+        comm.Alltoallv(s_msg, r_msg)
 
         temp_edges = None
 
@@ -89,40 +89,27 @@ class NodeDataLoader(ObjectBase):
         r = min(self.node_slit * (rank + 1), feats.shape[0])
         return feats[l:r]
 
-    def __gather_feat_metadata(self):
+    def __gather_feat_metadata(self, comm):
         rkeybuf, rkeybuf_len, addr, addr_len = _CAPI_DistV2MapRMAService(self)
         # gather rkey buffer
-        rkeybufs = self.comm.allgather(rkeybuf, rkeybuf_len)
+        rkeybufs = allgather(comm, rkeybuf, rkeybuf_len)
         # gather address
-        addrs = self.comm.allgather(addr, addr_len)
+        addrs = allgather(comm, addr, addr_len)
         return rkeybufs, addrs
 
     def __del__(self):
         # stop DataLoader, then
-        self.comm.mpi.barrier()
+        from mpi4py import MPI
+        MPI.COMM_WORLD.barrier()
         _CAPI_DistV2TermNodeDataLoader(self)
         del self.comm
 
-    def __init__(self, dataset, num_layers, edges, feats, labels, max_epoch, fanouts = None, batch_size = 1000, prefetch = 5, seed = 777, comm = MPI.COMM_WORLD):
-        self.comm = Communicator(comm)
-        self.num_layers = num_layers
-        self.labels = labels[:]
-        self.max_epoch = max_epoch
-        self.prefetch = prefetch
-        self.num_nodes = feats.shape[0]
-        print("num_nodes={}".format(self.num_nodes))
+    def __setup(self, edges, feats):
+        from mpi4py import MPI
+        self.comm = Communicator(MPI.COMM_WORLD)
         self.node_slit = (self.num_nodes + self.comm.size - 1) // self.comm.size
-        if fanouts is not None:
-            assert len(fanouts) == self.num_layers
-            self.fanouts = fanouts
-        else:
-            self.fanouts = [-1] * self.num_layers
-        self.batch_size = batch_size
         self.local_feats = self.__load_feats(self.comm.rank, self.node_slit, feats)
-        src, dst = self.__create_edgeshard(edges)
-        self.epoch = 0
-        self.seed = seed
-        self.dataset = dataset[:]
+        src, dst = self.__create_edgeshard(MPI.COMM_WORLD, edges)
         self.num_samples = len(self.dataset) // self.comm.size
         self.total_size = self.num_samples * self.comm.size
         self.num_batch = (self.num_samples + self.batch_size - 1) // self.batch_size
@@ -137,9 +124,27 @@ class NodeDataLoader(ObjectBase):
             nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(src))),
             nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(dst)))
         )
-        self.comm.create_endpoints()
-        rkeybufs, addrs = self.__gather_feat_metadata()
+        self.comm.create_endpoints(MPI.COMM_WORLD)
+        rkeybufs, addrs = self.__gather_feat_metadata(MPI.COMM_WORLD)
         _CAPI_DistV2PrepareRMAService(self, rkeybufs, addrs)
+
+    def __init__(self, dataset, num_layers, edges, feats, labels, max_epoch, fanouts = None, batch_size = 1000, prefetch = 5, seed = 777):
+        self.num_layers = num_layers
+        self.labels = labels[:]
+        self.dataset = dataset[:]
+        self.max_epoch = max_epoch
+        self.prefetch = prefetch
+        self.num_nodes = feats.shape[0]
+        if fanouts is not None:
+            assert len(fanouts) == self.num_layers
+            self.fanouts = fanouts
+        else:
+            self.fanouts = [-1] * self.num_layers
+        self.batch_size = batch_size
+        self.epoch = 0
+        self.seed = seed
+        self.__setup(edges, feats)
+        # launch
         _CAPI_DistV2LaunchNodeDataLoader(self)
         self.pre_iter = 0
         self.__reset()
@@ -169,7 +174,6 @@ class NodeDataLoader(ObjectBase):
     def __next__(self):
         if self.pre_iter < self.num_batch * self.max_epoch:
             self.__enqueue()
-        self.comm.mpi.barrier()
         if self.iter < self.num_batch:
             self.iter += 1
             _blocks, labels, feats = _CAPI_DistV2DequeueToNodeDataLoader(self)
