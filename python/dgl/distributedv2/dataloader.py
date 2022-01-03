100e0d206931d07d1759b08afb4a586859284d3c
KoyamaSohei <koyamaso0309@gmail.com>
2022-01-04 01:55:43 +0900

    add EdgeShard and batched alltoall()

--- python/dgl/distributedv2/dataloader.py
+++ python/dgl/distributedv2/dataloader.py
@@ -11,79 +11,87 @@ from .. import ndarray as nd
 from collections import deque
 
 __all__ = [
-    'NodeDataLoader'
+    'EdgeShard',
+    'NodeDataLoader',
 ]
 
-@register_object('distributedv2.NodeDataLoader')
-class NodeDataLoader(ObjectBase):
-    def __load_temp_edge(self, edges):
-        edge_slit = (edges.shape[0] + self.comm.size - 1) // self.comm.size
-        l = edge_slit * self.comm.rank
-        r = min(l + edge_slit, edges.shape[0])
-        return edges[l:r, :]
-    def __count_dest_rank(self, edges):
-        return np.unique(np.floor_divide(edges[:, 1], self.node_slit), return_counts=True)
-    """
-    `create_distgraph` creates hetero graph and shard nodes and edges.
-    shard policy is node's "Range".
-    let nodeX id as x.
-    nodeX is managed by rank=⌊x/⌈num_nodes/size⌉⌋ process.
-    let edgeAB as a -> b (a is src node id, b is dest node id).
-    edgeAB is managed by rank=⌊b/⌈num_nodes/size⌉⌋ process.
-    So each process knows incoming edge whose dest node is managed by self.
+"""
+`EdgeShard` creates hetero graph and shard nodes and edges.
+shard policy is node's "Range".
+let nodeX id as x.
+nodeX is managed by rank=⌊x/⌈num_nodes/size⌉⌋ process.
+let edgeAB as a -> b (a is src node id, b is dest node id).
+edgeAB is managed by rank=⌊b/⌈num_nodes/size⌉⌋ process.
+So each process knows incoming edge whose dest node is managed by self.
 
-    It is inefficient for all processes to scan all edges independently.
-    Do the following to reduce memory usage and execution times.
-    1. each process load temporary edges by load_temp_edge()
-    2. each process calculate where to shard temporary edges.
-    3. exchange temporary edges by Alltoallv()
-    """
-    def __create_edgeshard(self, comm, edges):
+It is inefficient for all processes to scan all edges independently.
+Do the following to reduce memory usage and execution times.
+1. each process load temporary edges by load_temp_edge()
+2. each process calculate where to shard temporary edges.
+3. exchange temporary edges by Alltoallv()
+"""
+class EdgeShard:
+    MAX_BUFFER_SIZE = 1<<20
+    def count_dest_rank(self, edges):
+        return np.unique(np.floor_divide(edges[:, 1], self.node_slit), return_counts=True)
+    def alltoall(self, comm, temp_edges):
         from mpi4py import MPI
-        temp_edges = self.__load_temp_edge(edges)
-        vals, cnts = self.__count_dest_rank(temp_edges)
-        s_counts = array('Q', [0] * self.comm.size)
+        vals, cnts = self.count_dest_rank(temp_edges)
+        s_counts = array('Q', [0] * self.size)
         for val, cnt in np.stack((vals, cnts), axis = 1):
             s_counts[val] = cnt
         # r_counts is how many edges this proc will fetch from others.
-        r_counts = array('Q', [0] * self.comm.size)
+        r_counts = array('Q', [0] * self.size)
         s_msg = [s_counts, 8, MPI.BYTE]
         r_msg = [r_counts, 8, MPI.BYTE]
         comm.Alltoall(s_msg, r_msg)
-
         # reuse s_counts as byte counts
-        s_counts = [c * (edges.itemsize * 2) for c in s_counts]
-
+        s_counts = [c * (temp_edges.itemsize * 2) for c in s_counts]
         # s_displs is s_counts offset 
-        s_displs = array('Q', [0] * self.comm.size)
-        for idx in range(1, self.comm.size):
+        s_displs = array('Q', [0] * self.size)
+        for idx in range(1, self.size):
             s_displs[idx] = s_displs[idx-1] + s_counts[idx-1]
-
         # sort by dest node id.
         temp_edges = temp_edges[temp_edges[:, 1].argsort()]
-
         # recv_edges is recv buffer.
-        recv_edges = np.zeros((sum(r_counts), 2), dtype=edges.dtype)
-
+        recv_edges = np.zeros((sum(r_counts), 2), dtype=temp_edges.dtype)
         # reuse r_counts as byte counts
-        r_counts = [c * (edges.itemsize * 2) for c in r_counts]
-
+        r_counts = [c * (temp_edges.itemsize * 2) for c in r_counts]
         # r_displs is r_counts offset
-        r_displs = array('Q', [0] * self.comm.size)
-        for idx in range(1, self.comm.size):
+        r_displs = array('Q', [0] * self.size)
+        for idx in range(1, self.size):
             r_displs[idx] = r_displs[idx-1] + r_counts[idx-1]
-
         s_msg = [temp_edges, (s_counts, s_displs), MPI.BYTE]
         r_msg = [recv_edges, (r_counts, r_displs), MPI.BYTE]
         comm.Alltoallv(s_msg, r_msg)
+        return recv_edges
 
-        temp_edges = None
+    def __init__(self, comm, edges, node_slit):
+        assert edges.shape[1] == 2
+        edges_len = edges.shape[0]
+        self.rank = comm.Get_rank()
+        self.size = comm.Get_size()
+        self.node_slit = node_slit
+        self.edge_slit = (edges_len + self.size - 1) // self.size
+        l = self.edge_slit * self.rank
+        r = min(l + self.edge_slit, edges_len)
+        batched_edges = [
+            self.alltoall(comm,
+                edges[batch_l:min(batch_l + self.MAX_BUFFER_SIZE, r), :]
+            )
+            for batch_l in range(l, r, self.MAX_BUFFER_SIZE)
+        ]
+        self.shard = np.concatenate(batched_edges)
+        self.shard = self.shard[np.lexsort((self.shard[:,0], self.shard[:,1]))]
+    @property
+    def src(self):
+        return self.shard[:, 0]
+    @property
+    def dst(self):
+        return self.shard[:, 1]
 
-        # sort by dest node id, then src node id.
-        recv_edges = recv_edges[np.lexsort((recv_edges[:,0], recv_edges[:,1]))]
-
-        return recv_edges[:, 0], recv_edges[:, 1]
-
+@register_object('distributedv2.NodeDataLoader')
+class NodeDataLoader(ObjectBase):
     def __load_feats(self, rank, node_slit, feats):
         l = self.node_slit * rank
         r = min(self.node_slit * (rank + 1), feats.shape[0])
@@ -119,8 +127,8 @@ class NodeDataLoader(ObjectBase):
             local_comm = MPI.COMM_WORLD.Split(color, key)
         self.comm = Communicator(local_comm)
         self.node_slit = (self.num_nodes + self.comm.size - 1) // self.comm.size
+        self.shard = EdgeShard(local_comm, edges, self.node_slit)
         self.local_feats = self.__load_feats(self.comm.rank, self.node_slit, feats)
-        src, dst = self.__create_edgeshard(local_comm, edges)
         self.num_samples = len(self.dataset) // self.world_size
         self.total_size = self.num_samples * self.world_size
         self.num_batch = (self.num_samples + self.batch_size - 1) // self.batch_size
@@ -132,8 +140,8 @@ class NodeDataLoader(ObjectBase):
             self.num_nodes,
             self.fanouts,
             nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(self.local_feats))),
-            nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(src))),
-            nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(dst)))
+            nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(self.shard.src))),
+            nd.from_dlpack(F.zerocopy_to_dlpack(F.zerocopy_from_numpy(self.shard.dst)))
         )
         self.comm.create_endpoints(local_comm)
         rkeybufs, addrs = self.__gather_feat_metadata(local_comm)
